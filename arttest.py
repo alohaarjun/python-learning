98960c6ae087bf8dd89b7bf1d1f90b7a4a76f47f
alohaarjun <alohaarjun@gmail.com>
2026-07-28 12:18:53 +0100

    Clean up arttest.py by removing old code
    Removed commented-out code and consolidated user input prompts.

--- arttest.py
+++ arttest.py
@@ -4,22 +4,6 @@ quest = input("What's your quest? ")
 color = input("What's your favorite color? ")
 print(f"So your name is {name}, your quest is {quest}, and your favorite color is {color}.")
 
-#Aditya's Code (1st try WRONG)
-name = input("What is your name ?")
-print("My name is + name +   ".")
+#Aditya's Code 
 
-quest = input("What is your quest?")
-print("My quest is + quest + ".")
-
-color = input("What is your favorite color")
-print("My favorite color is + color + ".")
-
-#Corrected Version
-name = input("What is your name ?")
-print("My name is " + name +   ".")
-
-quest = input("What is your quest?")
-print("My quest is " + quest + ".")
-
-color = input("What is your favorite color?")
-print("My favorite color is " + color + ".")
+#Aarav's Code 
