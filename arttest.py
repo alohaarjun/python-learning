e6076093f26faee87a96e72e85dcff249d6b51e7
Aditya570749 <adityasomasoma@gmail.com>
2026-07-28 11:42:10 +0100

    Remove print statements from arttest.py

--- arttest.py
+++ arttest.py
@@ -1,6 +1,30 @@
-ex_10 = "tungtungtungsahurrr"
-print(ex_10[0:5])  # Out
 
-print
-print(ex_10[5:10])  # Output: ungtu
-print(ex_10[10:15])  # Output: ngsah
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
