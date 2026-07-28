d0e63b08eb613533796663eb05ed81fb21315c42
sweatycoding <aaravaurathesweat@gmail.com>
2026-07-28 12:29:16 +0100

    Update arttest.py

--- arttest.py
+++ arttest.py
@@ -1,5 +1,8 @@
 # My Code
 
-#Aditya's Code 
+# Aditya's Code 
 
-#Aarav's Code 
+# Aarav's Code
+fav_mum =  input("What is your favourite number?")
+print("Your favourite  number is  " + fav_num  + ".")
+print(type(fav_num))
