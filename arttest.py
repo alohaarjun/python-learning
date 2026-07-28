5fc65d84fa947b0abd3955382101ca7fcb07966a
Aditya570749 <adityasomasoma@gmail.com>
2026-07-28 12:08:16 +0100

    Refactor user input and output statements
    Refactor input prompts and print statements for clarity.

--- arttest.py
+++ arttest.py
@@ -4,4 +4,13 @@ quest = input("What's your quest? ")
 color = input("What's your favorite color? ")
 print(f"So your name is {name}, your quest is {quest}, and your favorite color is {color}.")
 
-# Aditya's Code
+name = input("What is your name ?")
+print("My name is + name +   ".")
+
+quest = input("What is your quest?")
+print("My quest is + quest + ".")
+
+color = input("What is your favorite color")
+print("My favorite color is + color + ".")
+
+
