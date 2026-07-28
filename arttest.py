39e5edbda2c7934fa2d307f3f2acbee26faaa865
alohaarjun <alohaarjun@gmail.com>
2026-07-28 12:19:09 +0100

    Remove input prompts from arttest.py
    Removed user input prompts and print statement.

--- arttest.py
+++ arttest.py
@@ -1,8 +1,4 @@
 # My Code
-name = input("What's your name? ")
-quest = input("What's your quest? ")
-color = input("What's your favorite color? ")
-print(f"So your name is {name}, your quest is {quest}, and your favorite color is {color}.")
 
 #Aditya's Code 
 
