186f0ad759ce48e22e8b70c9c1db006049ef98a8
alohaarjun <alohaarjun@gmail.com>
2026-08-06 12:22:56 +0100

    Implement compliment function in arttest.py
    Add a function to generate random compliments.

--- arttest.py
+++ arttest.py
@@ -1,4 +1,19 @@
 # My Code
+def compliment():
+    import random
+    random_compliments = [
+        "You have a great sense of humor!",
+        "You are very tungtungsacool.",
+        "Everybody likes the records that you play.",
+        "You're really talented.",
+        "You have a great personality."
+        "You are almost as beautiful as Arjun"
+        "You have alpha sigma dominance"
+    ]
+    return random.choice(random_compliments)
+
+input_name = input("Enter your name: ")
+print(f"Hello, {input_name}!  {compliment()}")
 
 # Aditya's Code 
 
