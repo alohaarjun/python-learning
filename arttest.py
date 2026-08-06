8ff503b8a1f070e6ae4c9015392454ccc9346c07
Aditya570749 <adityasomasoma@gmail.com>
2026-08-06 12:12:54 +0100

    Fix indentation and formatting in arttest.py

--- arttest.py
+++ arttest.py
@@ -3,6 +3,6 @@
 # Aditya's Code 
 
 # Aarav's Code
-fav_num =  input("What is your favourite number?")
-print("Your favourite  number is  " + fav_num  + ".")
-print(type(fav_num))
+fav_num =  input("What is your favourite number?")            
+print("Your favourite  number is  " + fav_num  + ".")                
+print(type(fav_num))                                           
