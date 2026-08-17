04d3f803acf94487146b4eec96b4de6c3b73be13
Aditya570749 <adityasomasoma@gmail.com>
2026-08-17 11:43:23 +0100

    Update arttest.py

--- arttest.py
+++ arttest.py
@@ -16,6 +16,18 @@ input_name = input("Enter your name: ")
 print(f"Hello, {input_name}!  {compliment()}")
 
 # Aditya's Code 
+noun = input("enter a noun: ")
+noun2 = input("enter another noun: ")
+noun3 = input("add another noun big boy: ") 
+adjective = input("add the first adjective: ")
+verb = input("add another verb: ")
+adjective2 = input("add another adjective, then we can do it: ")
+noun4 = input("dont be sad, but unfortunately you have to add the last noun: ")
+adjective3 = input("the last adjective of today for you to enter sir arjun the tung: ")
+
+print(f"The gargantuan twin giants {noun} and {noun2} started chasing the {noun3} in an {adjective} manner!")
+print(f"Then suddenly they {verb}ed on {noun3} and started {adjective2}ing.")
+print(f"They then started a {noun4} and {adjective3}ed happily ever after.")
 
 # Aarav's Code
 fav_num =  input("What is your favourite number?")            
