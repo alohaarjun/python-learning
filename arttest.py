e9900dde63d27c733a90487dfe18cf6ee8585b54
Aditya570749 <adityasomasoma@gmail.com>
2026-08-17 11:58:18 +0100

    Remove story-related print statements
    Removed unnecessary print statements related to a story.

--- arttest.py
+++ arttest.py
@@ -9,8 +9,6 @@ area = length * width
 print(f"{area}cm²")
 
 
-print(f"The gargantuan twin giants {noun} and {noun2} started chasing the {noun3} in an {adjective} manner!")
-print(f"Then suddenly they {verb}ed on {noun3} and started {adjective2}ing.")
-print(f"They then started a {noun4} and {adjective3}ed happily ever after.")
+
 
 # Aarav's Code
